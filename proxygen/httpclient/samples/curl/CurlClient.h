0af6757f491436f72b468ca9c9cd1b8896466a90
vaz985 <vaz985@gmail.com>
2020-09-29 02:18:31 -0300

    Client can write to /dev/null

--- proxygen/httpclient/samples/curl/CurlClient.h
+++ proxygen/httpclient/samples/curl/CurlClient.h
@@ -81,6 +81,8 @@ class CurlClient
 
   bool saveResponseToFile(const std::string& outputFilename);
 
+  bool saveResponseToNull();
+
   static proxygen::HTTPHeaders parseHeaders(const std::string& headersString);
 
   // initial SSL related structures
